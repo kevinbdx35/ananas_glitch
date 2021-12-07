fda747b148125c522ecc2ea8989be04ddad67ebe
kevinbdx35 <k.bardoux@orange.fr>
2021-12-07 17:50:01 +0100

    clean the code

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ setInterval(function(){
 const glitch = document.getElementsByClassName('box');
 for(let i = 0; i < glitch.length; i++){
     glitch[i].style.left = Math.floor(Math.random()*100) + 'vw';
-    glitch[i].style.top = Math.floor(Math.random()*50) + 'vh';
+    glitch[i].style.top = Math.floor(Math.random()*500) + 'vh';
     glitch[i].style.width = Math.floor(Math.random()*200) + 'px';
     glitch[i].style.height = Math.floor(Math.random()*100) + 'px';
-    glitch[i].style.backgroundPosition = Math.floor(Math.random()*50) + 'px';
+    glitch[i].style.backgroundPosition = Math.floor(Math.random()*500) + 'px';
 }
 }, 200)
