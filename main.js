678978a0f5086626dcffdeba7212b252e2b5339a
kevinbdx35 <k.bardoux@orange.fr>
2021-12-07 17:40:41 +0100

    fix bug

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ setInterval(function(){
 const glitch = document.getElementsByClassName('box');
 for(let i = 0; i < glitch.length; i++){
     glitch[i].style.left = Math.floor(Math.random()*100) + 'vw';
-    glitch[i].style.top = Math.floor(Math.random()*100) + 'vh';
-    glitch[i].style.width = Math.floor(Math.random()*400) + 'px';
+    glitch[i].style.top = Math.floor(Math.random()*50) + 'vh';
+    glitch[i].style.width = Math.floor(Math.random()*200) + 'px';
     glitch[i].style.height = Math.floor(Math.random()*100) + 'px';
     glitch[i].style.backgroundPosition = Math.floor(Math.random()*50) + 'px';
 }
